1cb821a8c9355499da94c5234388eda3c5657f75
Fraser Patterson <fraser@snug.gy>
2022-02-22 01:00:33 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ Rscript -e "bookdown::render_book('index.Rmd', 'all')"
 mkdir -p _book/fr
 cd translations/fr/
 Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-cp -r _book/* ../../_book/ru/
+cp -r _book/* ../../_book/fr/
 
 # Hugarian Version
 mkdir -p _book/hu
