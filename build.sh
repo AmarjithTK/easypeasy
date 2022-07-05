5761695e1d324979adb570ae407ca5045d68b92f
Tim Budweg <timbudweg@gmail.com>
2022-07-05 22:45:47 +0200

    fr and hu do not work yet

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 # Build English version
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
 # Build German version
 mkdir -p _book/de
 cd translations/de/
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 cp -r _book/* ../../_book/de/
 
-# French Version
-mkdir -p _book/fr
-cd translations/fr/
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-cp -r _book/* ../../_book/fr/
+# # French Version
+# mkdir -p _book/fr
+# cd translations/fr/
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# cp -r _book/* ../../_book/fr/
 
-# Hugarian Version
-mkdir -p _book/hu
-cd translations/hu/
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-cp -r _book/* ../../_book/hu/
+# # Hugarian Version
+# mkdir -p _book/hu
+# cd translations/hu/
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# cp -r _book/* ../../_book/hu/
