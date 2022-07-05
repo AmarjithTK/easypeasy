cdf8e17ad18b3713e2396968f821921b3d5542a6
Tim Budweg <timbudweg@gmail.com>
2022-07-05 16:13:10 +0200

    translated index removed duplicates and added german language build

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 # Build English version
 Rscript -e "bookdown::render_book('index.Rmd', 'all')"
 
+# Build German version
+mkdir -p _book/de
+cd translations/de/
+Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+cp -r _book/* ../../_book/de/
+
 # French Version
 mkdir -p _book/fr
 cd translations/fr/
