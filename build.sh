e6ed441195514f5d1259d0b4a9c2e05f2a588e34
Tim Budweg <timbudweg@gmail.com>
2022-07-05 21:46:51 +0200

    experimenting with build.sh

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ cd translations/de/
 Rscript -e "bookdown::render_book('index.Rmd', 'all')"
 cp -r _book/* ../../_book/de/
 
-# French Version
-mkdir -p _book/fr
-cd translations/fr/
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-cp -r _book/* ../../_book/fr/
+# # French Version
+# mkdir -p _book/fr
+# cd translations/fr/
+# Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+# cp -r _book/* ../../_book/fr/
 
-# Hugarian Version
-mkdir -p _book/hu
-cd translations/hu/
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-cp -r _book/* ../../_book/hu/
+# # Hugarian Version
+# mkdir -p _book/hu
+# cd translations/hu/
+# Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+# cp -r _book/* ../../_book/hu/
