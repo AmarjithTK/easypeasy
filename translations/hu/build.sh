2c7e50785dd02edb11dbe8673aaad279d6e933ef
Tim Budweg <timbudweg@gmail.com>
2022-07-06 09:17:16 +0200

    deleted unnecessary build files

--- translations/hu/build.sh
+++
@@ -1,8 +0,0 @@
-# Build English version
-Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-
-# Translation Example: Build Russian Version
-#  mkdir -p _book/ru
-#  cd translations/ru/
-#  Rscript -e "bookdown::render_book('index.Rmd', 'all')"
-#  cp -r _book/* ../../_book/ru/
